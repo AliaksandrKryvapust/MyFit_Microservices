553508a8675476b426b23ab6049a4d519c8dc0f4
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-14 22:20:38 +0100

    init

---
+++ src/test/java/itacad/aliaksandrkryvapust/myfitapp/MyFitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package itacad.aliaksandrkryvapust.myfitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
