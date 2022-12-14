95e402e5d862eeb258e41b7795d67d2a29c0a561
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-14 22:34:07 +0100

    init

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/Constants.java
@@ -0,0 +1,12 @@
+package itacad.aliaksandrkryvapust.myfitapp.core;
+
+public class Constants {
+    public static final String CONTENT_TYPE = "application/json; charset=UTF-8";
+    public static final String ENCODING = "UTF-8";
+    public static final String ORDER_FINISH_DESCRIPTION = "Completed";
+    public static final String ORDER_START_DESCRIPTION = "Order accepted";
+    public static final String ORDER_DATA_ENTITY_GRAPH = "orderData.orderHistory";
+    public static final String COMPLETED_ORDER_ENTITY_GRAPH = "completedOrder.items";
+    public static final String MENU_ENTITY_GRAPH = "menu.items";
+    public static final String TICKET_ENTITY_GRAPH = "ticket.order.selectedItems";
+}
