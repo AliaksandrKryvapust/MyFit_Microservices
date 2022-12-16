85f46f5615ca73b61d1d31a3dc21b44bc2be050a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-16 23:14:59 +0100

    main was renamed

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/MyFitApp.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/MyFitApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyFitAppApplication {
+public class MyFitApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyFitAppApplication.class, args);
+        SpringApplication.run(MyFitApp.class, args);
     }
 
 }
