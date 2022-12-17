742cf9fd01e72408de02426c5dcdf1ef5ef20a54
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-17 23:12:41 +0100

    new manager

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/manager/api/IIngredientManager.java
@@ -0,0 +1,8 @@
+package itacad.aliaksandrkryvapust.myfitapp.manager.api;
+
+import itacad.aliaksandrkryvapust.myfitapp.core.dto.input.IngredientDtoInput;
+import itacad.aliaksandrkryvapust.myfitapp.core.dto.output.IngredientDtoOutput;
+
+public interface IIngredientManager extends IManager<IngredientDtoOutput, IngredientDtoInput>,
+IManagerUpdate<IngredientDtoOutput, IngredientDtoInput>, IManagerDelete {
+}
