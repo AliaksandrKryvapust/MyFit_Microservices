e87ccaa32ded41f0dd7ba5487d33f5c1fcaa916d
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-17 19:32:32 +0100

    hash code was added

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/IngredientKey.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/IngredientKey.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -15,4 +16,17 @@ import java.util.UUID;
 public class IngredientKey implements Serializable {
     private UUID productId;
     private UUID mealId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IngredientKey that = (IngredientKey) o;
+        return Objects.equals(getProductId(), that.getProductId()) && Objects.equals(getMealId(), that.getMealId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProductId(), getMealId());
+    }
 }
