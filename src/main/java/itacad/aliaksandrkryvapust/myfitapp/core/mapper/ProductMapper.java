4c0287332fa64cdeff78d8e7a0863b5a43b97b2d
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-17 21:44:04 +0100

    new mapper was added

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/mapper/ProductMapper.java
@@ -0,0 +1,35 @@
+package itacad.aliaksandrkryvapust.myfitapp.core.mapper;
+
+import itacad.aliaksandrkryvapust.myfitapp.core.dto.input.ProductDtoInput;
+import itacad.aliaksandrkryvapust.myfitapp.core.dto.output.ProductDtoOutput;
+import itacad.aliaksandrkryvapust.myfitapp.repository.entity.Product;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ProductMapper {
+        public Product inputMapping(ProductDtoInput productDtoInput) {
+        return Product.builder().title(productDtoInput.getTitle())
+                .calories(productDtoInput.getCalories())
+                .proteins(productDtoInput.getProteins())
+                .fats(productDtoInput.getFats())
+                .carbohydrates(productDtoInput.getCarbohydrates())
+                .weight(productDtoInput.getWeight()).build();
+    }
+
+    public ProductDtoOutput outputMapping(Product product) {
+        return ProductDtoOutput.builder()
+                .id(product.getId())
+                .title(product.getTitle())
+                .calories(product.getCalories())
+                .proteins(product.getProteins())
+                .fats(product.getFats())
+                .carbohydrates(product.getCarbohydrates())
+                .weight(product.getWeight())
+                .dtCreate(product.getDtCreate())
+                .dtUpdate(product.getDtUpdate())
+                .build();
+    }
+}
