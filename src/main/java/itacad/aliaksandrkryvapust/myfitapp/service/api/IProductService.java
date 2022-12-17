7f97fbd5b8e32c1045b40aedc0eb375f64f0d6a0
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-17 16:50:52 +0100

    new Service Interface was added

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/service/api/IProductService.java
@@ -0,0 +1,6 @@
+package itacad.aliaksandrkryvapust.myfitapp.service.api;
+
+import itacad.aliaksandrkryvapust.myfitapp.repository.entity.Product;
+
+public interface IProductService extends IService<Product>, IServiceUpdate<Product>, IServiceDelete {
+}
