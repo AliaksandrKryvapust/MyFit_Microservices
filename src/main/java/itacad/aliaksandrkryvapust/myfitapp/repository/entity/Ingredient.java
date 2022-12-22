8c866f8337f8ccab07b0a26d0ee7898375c553a9
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-22 14:22:28 +0100

    sql table was renamed

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/Ingredient.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/Ingredient.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "ingredients_new", schema = "app")
+@Table(name = "ingredients", schema = "app")
 public class Ingredient {
     @Id
     @GeneratedValue(generator = "uuid")
