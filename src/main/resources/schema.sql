27f9cbf54280c8b869c6d1fe5c581f572b846daa
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-22 14:23:05 +0100

    table script creation was added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,89 @@
+CREATE SCHEMA my_fit
+    AUTHORIZATION postgres;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE app.products
+(
+    id uuid,
+    title character varying(100) NOT NULL,
+    calories bigint NOT NULL,
+    proteins double precision NOT NULL,
+    fats double precision NOT NULL,
+    carbohydrates double precision NOT NULL,
+    weight bigint NOT NULL,
+    dt_create timestamp without time zone NOT NULL DEFAULT now(),
+    dt_update timestamp without time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS app.products
+    OWNER to postgres;
+
+CREATE TABLE app.ingredients
+(
+    id uuid,
+    meal_id uuid,
+    product_id uuid,
+    weight bigint NOT NULL,
+    dt_create timestamp without time zone NOT NULL DEFAULT now(),
+    dt_update timestamp without time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS app.ingredients
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS app.ingredients
+    ADD CONSTRAINT "meal_FK" FOREIGN KEY (meal_id)
+        REFERENCES app.meal (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
+
+ALTER TABLE IF EXISTS app.ingredients
+    ADD CONSTRAINT "products_FK" FOREIGN KEY (product_id)
+        REFERENCES app.products (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
+
+CREATE TABLE app.meal
+(
+    id uuid,
+    title character varying(100) NOT NULL,
+    dt_create timestamp without time zone NOT NULL DEFAULT now(),
+    dt_update timestamp without time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS app.meal
+    OWNER to postgres;
+
+CREATE TABLE app.records
+(
+    id uuid,
+    meal_id uuid,
+    product_id uuid,
+    weight bigint NOT NULL,
+    dt_create timestamp without time zone NOT NULL DEFAULT now(),
+    dt_supply timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS app.records
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS app.records
+    ADD CONSTRAINT "records_meal_FK" FOREIGN KEY (meal_id)
+        REFERENCES app.meal (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
+
+ALTER TABLE IF EXISTS app.records
+    ADD CONSTRAINT "records_product_FK" FOREIGN KEY (product_id)
+        REFERENCES app.products (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID;
