01c631c7d1d92aba1117eba7f544681202603eee
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-22 23:20:02 +0100

    new JwtFilter was added

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/controller/filter/JwtFilter.java
@@ -0,0 +1,67 @@
+package itacad.aliaksandrkryvapust.myfitapp.controller.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import itacad.aliaksandrkryvapust.myfitapp.controller.utils.JwtTokenUtil;
+import itacad.aliaksandrkryvapust.myfitapp.service.JwtUserDetailsService;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    public JwtFilter(JwtUserDetailsService jwtUserDetailsService, JwtTokenUtil jwtTokenUtil) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader(AUTHORIZATION);
+        if (requestTokenHeader.startsWith("Bearer ")) {
+            String jwtToken = requestTokenHeader.substring(7);
+            try {
+                String username = jwtTokenUtil.getUsername(jwtToken);
+                if (!username.isBlank() && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+                    if (jwtTokenUtil.validate(jwtToken, userDetails)) {
+                        UsernamePasswordAuthenticationToken authenticationToken =
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
+                }
+            } catch (MalformedJwtException e) {
+                logger.error("JWT token is invalid" + e.getMessage());
+            } catch (UnsupportedJwtException e) {
+                logger.error("Unsupported JWT token" + e.getMessage());
+            } catch (ExpiredJwtException e) {
+                logger.error("JWT token is expired");
+            } catch (IllegalArgumentException e) {
+                logger.error("Unable to fetch JWT token");
+            }
+        } else {
+            logger.warn("JWT token does not start with Bearer string");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
