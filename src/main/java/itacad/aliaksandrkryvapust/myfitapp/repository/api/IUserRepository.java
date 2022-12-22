ac8a96cff0733cf0993d74706a4b2d04ac55317c
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-22 22:42:54 +0100

    user entity repository was modified

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/api/IUserRepository.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/api/IUserRepository.java
@@ -1,11 +1,14 @@
 package itacad.aliaksandrkryvapust.myfitapp.repository.api;
 
+import itacad.aliaksandrkryvapust.myfitapp.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
 
-//@Repository
-//public interface IUserRepository extends JpaRepository<User, Long> {
-//    User findByUsername(String username);
-//}
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+}
 
