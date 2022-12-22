b3da136feb99107d00a9c743135c5fb4bd5beca0
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-22 20:10:06 +0100

    user entity was modified

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/User.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/repository/entity/User.java
@@ -1,46 +1,43 @@
 package itacad.aliaksandrkryvapust.myfitapp.repository.entity;
 
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.io.Serializable;
+import java.util.UUID;
 
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Entity
-//@Table(name = "users", schema = "app")
-//public class User implements Serializable {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//    @Setter
-//    @Column(nullable = false, unique = true)
-//    private String username;
-//    @Setter
-//    @Column(nullable = false)
-//    private String password;
-//    @Setter
-//    @Column(nullable = false, unique = true)
-//    private String email;
-//    @Setter
-//    private Integer height;
-//    @Setter
-//    private Double weight;
-//    @Setter
-//    private Integer age;
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", username='" + username + '\'' +
-//                ", password='" + password + '\'' +
-//                ", email='" + email + '\'' +
-//                ", height=" + height +
-//                ", weight=" + weight +
-//                ", age=" + age +
-//                '}';
-//    }
-//}
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "app")
+public class User{
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    @Setter
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Setter
+    @Column(nullable = false)
+    private String password;
+    @Setter
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
