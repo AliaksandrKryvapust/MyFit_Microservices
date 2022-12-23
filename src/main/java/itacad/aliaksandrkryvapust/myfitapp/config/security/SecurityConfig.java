32bd41894b15b13f51adebe4ad09e190d6dd23c8
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-23 15:06:42 +0100

    new authentication manager was added

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
@@ -5,7 +5,9 @@ import itacad.aliaksandrkryvapust.myfitapp.repository.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,6 +31,11 @@ public class SecurityConfig {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
