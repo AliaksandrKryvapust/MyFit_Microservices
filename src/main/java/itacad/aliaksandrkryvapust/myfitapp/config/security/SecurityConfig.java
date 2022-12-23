f9b147dabe584ec75147d160c46c4ea821bd28dd
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-23 11:55:24 +0100

    spring security config was modified

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
@@ -1,44 +1,48 @@
 package itacad.aliaksandrkryvapust.myfitapp.config.security;
 
+import itacad.aliaksandrkryvapust.myfitapp.controller.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user1")
-//                .password(passwordEncoder()
-//                        .encode("user1Pass"))
-//                .authorities("ROLE_USER");
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .and()
-//                .logout().permitAll()
-//                .and()
-//                .httpBasic();
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public MyUserDetailsService userDetailsService() {
-//        return new MyUserDetailsService();
-//    }
-//}
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserDetailsService jwtUserDetailsService;
+    private final JwtFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService jwtUserDetailsService, JwtFilter jwtFilter) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/public/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
