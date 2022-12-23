7e15ada526d356431397cb871b5b70df23bcde14
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-23 16:30:11 +0100

    user dto was modified for login

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/dto/output/UserDtoOutput.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/dto/output/UserDtoOutput.java
@@ -2,13 +2,9 @@ package itacad.aliaksandrkryvapust.myfitapp.core.dto.output;
 
 import lombok.*;
 
-//@Builder
-//@Data
-//public class UserDtoOutput {
-//    private final @NonNull Long id;
-//    private final @NonNull String username;
-//    private final @NonNull String email;
-//    private final Integer height;
-//    private final Double weight;
-//    private final Integer age;
-//}
+@Builder
+@Data
+public class UserDtoOutput {
+    private final @NonNull String username;
+    private final @NonNull String token;
+}
