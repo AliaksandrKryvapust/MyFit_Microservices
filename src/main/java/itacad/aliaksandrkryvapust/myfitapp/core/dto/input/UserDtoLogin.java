0ec059b1e7397e7ef4a36a2c46508cdaea16f2aa
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-23 16:30:48 +0100

    new dto for login

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/dto/input/UserDtoLogin.java
@@ -0,0 +1,20 @@
+package itacad.aliaksandrkryvapust.myfitapp.core.dto.input;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Builder
+@Data
+@Jacksonized
+public class UserDtoLogin {
+    @NotNull(message = "username cannot be null")
+    @Size(min = 2, max = 50, message = "username should contain from 2 to 50 letters")
+    private final String username;
+    @NotNull(message = "pizza info cannot be null")
+    @Size(min = 2, max = 200, message = "username should contain from 2 to 200 letters")
+    private final String password;
+}
