8130f80149a8f31048a10327eedbb6206e6c317c
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-24 15:41:06 +0100

    user dto login was modified

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/dto/input/UserDtoLogin.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/dto/input/UserDtoLogin.java
@@ -15,6 +15,6 @@ public class UserDtoLogin {
     @Size(min = 2, max = 50, message = "username should contain from 2 to 50 letters")
     private final String username;
     @NotNull(message = "pizza info cannot be null")
-    @Size(min = 2, max = 200, message = "username should contain from 2 to 200 letters")
+    @Size(min = 2, max = 16, message = "username should contain from 2 to 16 letters")
     private final String password;
 }
