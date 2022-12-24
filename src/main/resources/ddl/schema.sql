4050b72ed2e2563c760fd16f39dac4b254e8d414
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-24 15:29:33 +0100

    table row was added

--- src/main/resources/ddl/schema.sql
+++ src/main/resources/ddl/schema.sql
@@ -63,11 +63,12 @@ ALTER TABLE IF EXISTS app.records
 
 CREATE TABLE IF NOT EXISTS app.users
 (
-    id       uuid,
-    username character varying(20) NOT NULL,
-    password character varying(50) NOT NULL,
-    email    character varying(50) NOT NULL,
-    role     character varying(20) NOT NULL,
+    id        uuid,
+    username  character varying(20)       NOT NULL,
+    password  character varying(200)       NOT NULL,
+    email     character varying(50)       NOT NULL,
+    role      character varying(20)       NOT NULL,
+    dt_update timestamp without time zone NOT NULL DEFAULT now(),
     PRIMARY KEY (id)
 );
 
