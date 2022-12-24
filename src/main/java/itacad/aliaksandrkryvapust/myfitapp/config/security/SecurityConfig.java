85f410801abacc06cc78ecd8e85ed2450f8028e1
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-24 19:19:00 +0100

    new bean foe encoder

--- src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/config/security/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -52,5 +54,10 @@ public class SecurityConfig {
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
