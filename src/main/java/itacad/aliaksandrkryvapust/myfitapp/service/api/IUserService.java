069eaa7396be0b37a4117a6c155cefe8a87fb98a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-24 16:05:32 +0100

    user service was added

---
+++ src/main/java/itacad/aliaksandrkryvapust/myfitapp/service/api/IUserService.java
@@ -0,0 +1,6 @@
+package itacad.aliaksandrkryvapust.myfitapp.service.api;
+
+import itacad.aliaksandrkryvapust.myfitapp.repository.entity.User;
+
+public interface IUserService extends IService<User>, IServiceUpdate<User>{
+}
