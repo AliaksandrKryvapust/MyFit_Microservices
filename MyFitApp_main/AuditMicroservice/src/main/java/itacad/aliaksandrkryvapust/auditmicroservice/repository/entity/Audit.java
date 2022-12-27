6ab54a49372a20c28846dc7ee76d9cb08b69af75
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-27 22:06:56 +0100

    new entity was added

---
+++ MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/repository/entity/Audit.java
@@ -0,0 +1,27 @@
+package itacad.aliaksandrkryvapust.auditmicroservice.repository.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenerationTime;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "audit")
+public class Audit {
+    @Id
+    private String id;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    private String text;
+    @Enumerated(EnumType.STRING)
+    private Type type;
+    @org.hibernate.annotations.Generated(GenerationTime.INSERT)
+    private Instant dtCreate;
+}
