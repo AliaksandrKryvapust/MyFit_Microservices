b2498af4ca978be7d73f236e539b07922a1cec8d
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-27 15:59:53 +0100

    new modules

--- AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/core/Constants.java
+++
@@ -1,6 +0,0 @@
-package itacad.aliaksandrkryvapust.auditmicroservice.core;
-
-public class Constants {
-    public static final String CONTENT_TYPE = "application/json; charset=UTF-8";
-    public static final String ENCODING = "UTF-8";
-}
