afcd64b03413dc09841b17d471afff4a730606ea
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-27 22:55:50 +0100

    audit service was modified

--- MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/service/AuditService.java
+++ MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/service/AuditService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -20,6 +21,7 @@ public class AuditService implements IAuditService {
     }
 
     @Override
+    @Transactional
     public Audit save(Audit audit) {
         return this.auditRepository.save(audit);
     }
