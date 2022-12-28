6f6c72160f7b94e988f3a8f19584fcaaa738adb5
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-28 15:50:16 +0100

    new constants were added

--- MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/Constants.java
+++ MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/Constants.java
@@ -2,7 +2,10 @@ package itacad.aliaksandrkryvapust.myfitapp.core;
 
 public class Constants {
     public static final String CONTENT_TYPE = "application/json; charset=UTF-8";
+    public static final String CONTENT = "Content-Type";
     public static final String ENCODING = "UTF-8";
+    public static final String AUDIT_URI = "http://localhost:8081/api/v1/audit";
+    public static final String TOKEN_HEADER = "x-access-token";
     public static final String ORDER_FINISH_DESCRIPTION = "Completed";
     public static final String ORDER_START_DESCRIPTION = "Order accepted";
     public static final String MENU_ENTITY_GRAPH = "menu.items";
