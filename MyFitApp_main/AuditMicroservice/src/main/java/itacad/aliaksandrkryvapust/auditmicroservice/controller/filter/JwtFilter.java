166363064553e53aac9f080fc6ec896fec4b6975
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-28 17:21:48 +0100

    jwt filter was modified

--- MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/controller/filter/JwtFilter.java
+++ MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/controller/filter/JwtFilter.java
@@ -1,20 +1,15 @@
 package itacad.aliaksandrkryvapust.auditmicroservice.controller.filter;
 
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import itacad.aliaksandrkryvapust.auditmicroservice.controller.utils.JwtTokenUtil;
-import itacad.aliaksandrkryvapust.auditmicroservice.service.JwtUserDetailsService;
+import itacad.aliaksandrkryvapust.auditmicroservice.core.dto.TokenValidationDto;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -22,48 +17,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static itacad.aliaksandrkryvapust.auditmicroservice.core.Constants.TOKEN_HEADER;
+import static itacad.aliaksandrkryvapust.auditmicroservice.core.Constants.TOKEN_VERIFICATION_URI;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
-    private final JwtUserDetailsService jwtUserDetailsService;
-    private final JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    public JwtFilter(JwtUserDetailsService jwtUserDetailsService, JwtTokenUtil jwtTokenUtil) {
-        this.jwtUserDetailsService = jwtUserDetailsService;
-        this.jwtTokenUtil = jwtTokenUtil;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String requestTokenHeader = request.getHeader(AUTHORIZATION);
-        if (StringUtils.startsWithIgnoreCase(requestTokenHeader, "Bearer ")) {
-            String jwtToken = requestTokenHeader.substring(7);
-            try {
-                String username = jwtTokenUtil.getUsername(jwtToken);
-                if (!username.isBlank() && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
-                    if (jwtTokenUtil.validate(jwtToken, userDetails)) {
-                        UsernamePasswordAuthenticationToken authenticationToken =
-                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        WebClient client = WebClient.create(TOKEN_VERIFICATION_URI);
+        TokenValidationDto resp = client.get()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(TOKEN_HEADER, requestTokenHeader)
+                .exchangeToMono((r) -> {
+                    if (r.statusCode().equals(HttpStatus.OK)) {
+                        return r.bodyToMono(TokenValidationDto.class);
+                    } else if (r.statusCode().is4xxClientError()) {
+                        throw new DataIntegrityViolationException("Bad request");
+                    } else {
+                        throw new RuntimeException("Failed to get token data");
                     }
-                }
-            } catch (MalformedJwtException e) {
-                logger.error("JWT token is invalid" + e.getMessage());
-            } catch (UnsupportedJwtException e) {
-                logger.error("Unsupported JWT token" + e.getMessage());
-            } catch (ExpiredJwtException e) {
-                logger.error("JWT token is expired");
-            } catch (IllegalArgumentException e) {
-                logger.error("Unable to fetch JWT token");
-            }
+                }).block();
+        if (resp.getIsAuthenticated()) {
+            filterChain.doFilter(request, response);
         } else {
-            logger.warn("JWT token does not start with Bearer string");
+            logger.warn("Access denied");
         }
-        filterChain.doFilter(request, response);
     }
 }
