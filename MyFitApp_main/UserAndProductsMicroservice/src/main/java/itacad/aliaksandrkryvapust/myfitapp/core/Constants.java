8ff7e7a963f25c783be9cf9c9b1269948e171f54
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-28 22:06:35 +0100

    header name was changed

--- MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/Constants.java
+++ MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/Constants.java
@@ -3,7 +3,7 @@ package itacad.aliaksandrkryvapust.myfitapp.core;
 public class Constants {
     public static final String ENCODING = "UTF-8";
     public static final String AUDIT_URI = "http://localhost:8081/api/v1/audit";
-    public static final String TOKEN_HEADER = "x-access-token";
+    public static final String TOKEN_HEADER = "audit_access_token";
     public static final String ORDER_FINISH_DESCRIPTION = "Completed";
     public static final String ORDER_START_DESCRIPTION = "Order accepted";
     public static final String MENU_ENTITY_GRAPH = "menu.items";
