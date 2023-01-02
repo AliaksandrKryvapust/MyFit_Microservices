7255b836f3c02d3e50559a51070201b674449e57
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-02 15:46:36 +0100

    new Service for Report was added

---
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/service/api/IReportService.java
@@ -0,0 +1,6 @@
+package aliaksandrkryvapust.reportmicroservice.service.api;
+
+import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
+
+public interface IReportService extends IService<Report> {
+}
