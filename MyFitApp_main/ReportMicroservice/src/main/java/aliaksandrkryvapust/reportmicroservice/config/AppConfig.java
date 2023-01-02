c122d1f682c1d06d591c68cbf9083ee8d6689dd8
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-02 18:37:14 +0100

    new enum converter was added

--- MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/config/AppConfig.java
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/config/AppConfig.java
@@ -1,12 +1,14 @@
 package aliaksandrkryvapust.reportmicroservice.config;
 
 
+import aliaksandrkryvapust.reportmicroservice.controller.validation.StringToTypeConverter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -31,5 +33,10 @@ public class AppConfig implements WebMvcConfigurer {
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
                 .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE).build();
         converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry){
+        registry.addConverter(new StringToTypeConverter());
     }
 }
