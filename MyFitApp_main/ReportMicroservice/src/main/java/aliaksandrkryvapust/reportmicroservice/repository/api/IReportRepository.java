5f1affece8dd247cedd30c8c69fb53721ba31510
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-03 16:50:08 +0100

    new method was added

--- MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/repository/api/IReportRepository.java
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/repository/api/IReportRepository.java
@@ -1,11 +1,15 @@
 package aliaksandrkryvapust.reportmicroservice.repository.api;
 
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
+import aliaksandrkryvapust.reportmicroservice.repository.entity.Status;
+import aliaksandrkryvapust.reportmicroservice.repository.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IReportRepository extends JpaRepository<Report, UUID> {
+    Optional<Report> findByStatusAndType(Status status, Type type);
 }
