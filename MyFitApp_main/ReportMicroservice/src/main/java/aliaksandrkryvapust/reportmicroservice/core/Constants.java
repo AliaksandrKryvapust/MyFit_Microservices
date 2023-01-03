f1108ec038ad66604cde1e7271226ce0eb563176
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-03 16:50:41 +0100

    constant list was updated

--- MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/core/Constants.java
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/core/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final String CONTENT_TYPE = "application/json; charset=UTF-8";
     public static final String ENCODING = "UTF-8";
     public static final String TOKEN_VERIFICATION_URI = "http://localhost:8080/api/v1/validateToken";
+    public static final String JOB_IMPORT_URI = "http://localhost:8080//api/v1/journal/food/export";
     public static final String TOKEN_HEADER = "audit_access_token";
 }
