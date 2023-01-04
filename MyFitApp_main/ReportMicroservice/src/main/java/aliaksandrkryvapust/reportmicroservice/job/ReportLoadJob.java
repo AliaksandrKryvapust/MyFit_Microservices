5ef6d77e58f22ae51fef0265aa6c1185ee09b711
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-04 19:45:11 +0100

    methods to get byte array was added

--- MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/job/ReportLoadJob.java
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/job/ReportLoadJob.java
@@ -1,10 +1,12 @@
 package aliaksandrkryvapust.reportmicroservice.job;
 
 import aliaksandrkryvapust.reportmicroservice.core.dto.job.RecordDto;
+import aliaksandrkryvapust.reportmicroservice.core.mapper.poi.XlsxRecordMapper;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Status;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Type;
 import aliaksandrkryvapust.reportmicroservice.service.api.IReportService;
+import aliaksandrkryvapust.reportmicroservice.service.api.IXlsxRecordService;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -27,9 +29,13 @@ import static aliaksandrkryvapust.reportmicroservice.core.Constants.TOKEN_HEADER
 public class ReportLoadJob implements Job {
     private static final String jwtSecret = "NDQ1ZjAzNjQtMzViZi00MDRjLTljZjQtNjNjYWIyZTU5ZDYw";
     private final IReportService reportService;
+    private final IXlsxRecordService xlsxRecordService;
+    private final XlsxRecordMapper recordMapper;
 
-    public ReportLoadJob(IReportService reportService) {
+    public ReportLoadJob(IReportService reportService, IXlsxRecordService xlsxRecordService, XlsxRecordMapper recordMapper) {
         this.reportService = reportService;
+        this.xlsxRecordService = xlsxRecordService;
+        this.recordMapper = recordMapper;
     }
 
     @Override
@@ -51,7 +57,7 @@ public class ReportLoadJob implements Job {
             report = this.reportService.get(id);
             List<RecordDto> records = resp.blockOptional().orElseThrow();
             log.info("Data from response was extracted");
-            // TODO map list to a file
+            byte[] convertedFile = xlsxRecordService.getRecordXlsData(recordMapper.listInputMapping(records));
             this.setProgressStatus(report, Status.DONE, "Report Job finished");
         } catch (Exception e) {
             log.error(e.getMessage());
