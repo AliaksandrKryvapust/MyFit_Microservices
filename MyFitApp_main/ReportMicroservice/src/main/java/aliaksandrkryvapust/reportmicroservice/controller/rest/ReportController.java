2f988394c6dfc3504750cb1ef54b6d9affc9f264
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-06 20:13:23 +0100

    new method to get file was added to Controller

--- MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/controller/rest/ReportController.java
+++ MyFitApp_main/ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/controller/rest/ReportController.java
@@ -1,18 +1,23 @@
 package aliaksandrkryvapust.reportmicroservice.controller.rest;
 
 import aliaksandrkryvapust.reportmicroservice.core.dto.ParamsDto;
-import aliaksandrkryvapust.reportmicroservice.core.dto.ReportDtoOutput;
 import aliaksandrkryvapust.reportmicroservice.core.dto.pages.PageDtoOutput;
 import aliaksandrkryvapust.reportmicroservice.manager.api.IReportManager;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Type;
+import org.apache.poi.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @RestController
@@ -32,9 +37,21 @@ public class ReportController {
         return ResponseEntity.ok(reportManager.get(pageable));
     }
 
-    @GetMapping("/{uuid}/export")
-    protected ResponseEntity<ReportDtoOutput> get(@PathVariable UUID uuid) {
-        return ResponseEntity.ok(reportManager.get(uuid));
+    @RequestMapping(value = "/{uuid}/export", method = RequestMethod.GET)
+    public void export(@PathVariable UUID uuid, HttpServletResponse response) {
+        byte[] output = reportManager.getReportFile(uuid);
+        try (InputStream data = new ByteArrayInputStream(output)) {
+            this.prepareHeadersAndOutputStream(uuid, response, data);
+            response.flushBuffer();
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка формирования выгрузки отчёта");
+        }
+    }
+
+    private void prepareHeadersAndOutputStream(UUID uuid, HttpServletResponse response, InputStream data) throws IOException {
+        response.addHeader(HttpHeaders.CONTENT_TYPE, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + uuid.toString() + ".xlsx");
+        IOUtils.copy(data, response.getOutputStream());
     }
 
     @PostMapping("/{type}")
