a8a4d9ca0743aabc54007f4c5edb5442d329fc68
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-08 17:07:13 +0100

    new mapper for Token

--- MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/mapper/TokenMapper.java
+++ MyFitApp_main/UserAndProductsMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/core/mapper/TokenMapper.java
@@ -2,7 +2,12 @@ package itacad.aliaksandrkryvapust.myfitapp.core.mapper;
 
 import itacad.aliaksandrkryvapust.myfitapp.repository.entity.EmailToken;
 import itacad.aliaksandrkryvapust.myfitapp.repository.entity.User;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class TokenMapper {
 
     public EmailToken inputMapping(User user, String token){
