2ab31ccb9fb68923532cf6acf8a4b0087dc39382
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-08 21:46:23 +0100

    name fix

--- MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/UserMicroserviceApplication.java
+++ MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/myfitapp/UserMicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserMicroservice {
+public class UserMicroserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserMicroservice.class, args);
+        SpringApplication.run(UserMicroserviceApplication.class, args);
     }
 
 }
