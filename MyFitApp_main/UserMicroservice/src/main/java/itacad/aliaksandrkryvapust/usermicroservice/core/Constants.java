2c35a0c03e90ab3825f031a8b6d284869dfa9118
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-08 22:31:21 +0100

    few constants were removed

--- MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/Constants.java
+++ MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/Constants.java
@@ -4,8 +4,6 @@ public class Constants {
     public static final String ENCODING = "UTF-8";
     public static final String AUDIT_URI = "http://localhost:8081/api/v1/audit";
     public static final String TOKEN_HEADER = "audit_access_token";
-    public static final int GMAIL_SMTP_PORT = 587;
-    public static final String GMAIL_SMTP_HOST = "smtp.gmail.com";
     public static final String MAIL_SUBJECT = "Registration Confirmation";
     public static final String TICKET_ENTITY_GRAPH = "ticket.order.selectedItems";
 }
