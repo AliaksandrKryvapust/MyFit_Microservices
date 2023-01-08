1f6ce7e461bf5246f2baf5165d2db2302e63e8c1
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-08 23:28:50 +0100

    email creation pattern was changed

--- MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/controller/listener/RegistrationListener.java
+++ MyFitApp_main/UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/controller/listener/RegistrationListener.java
@@ -6,9 +6,8 @@ import itacad.aliaksandrkryvapust.usermicroservice.manager.api.ITokenManager;
 import itacad.aliaksandrkryvapust.usermicroservice.repository.entity.User;
 import lombok.NonNull;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.MessageSource;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -18,12 +17,10 @@ import static itacad.aliaksandrkryvapust.usermicroservice.core.Constants.MAIL_SU
 @Component
 public class RegistrationListener implements ApplicationListener<EmailVerificationEvent> {
     private final ITokenManager tokenManager;
-    private final MessageSource messageSource;
-    private final JavaMailSender javaMailSender;
+    private final JavaMailSenderImpl javaMailSender;
 
-    public RegistrationListener(ITokenManager tokenManager, MessageSource messageSource, JavaMailSender javaMailSender) {
+    public RegistrationListener(ITokenManager tokenManager, JavaMailSenderImpl javaMailSender) {
         this.tokenManager = tokenManager;
-        this.messageSource = messageSource;
         this.javaMailSender = javaMailSender;
     }
 
@@ -33,7 +30,7 @@ public class RegistrationListener implements ApplicationListener<EmailVerificati
             User user = event.getUser();
             String token = UUID.randomUUID().toString();
             tokenManager.saveToken(user, token);
-            SimpleMailMessage email = createEmail(event, user, token);
+            SimpleMailMessage email = createEmail(user, token);
             javaMailSender.send(email);
         } catch (Exception e) {
             throw new EmailSendException(e.getMessage(), e);
@@ -41,10 +38,10 @@ public class RegistrationListener implements ApplicationListener<EmailVerificati
 
     }
 
-    private SimpleMailMessage createEmail(EmailVerificationEvent event, User user, String token) {
+    private SimpleMailMessage createEmail(User user, String token) {
         String addressToSend = user.getEmail();
-        String url = event.getAppUrl() + "/registration/confirm?token=" + token;
-        String message = messageSource.getMessage("message.regSucc", null, event.getLocale());
+        String url = "/api/v1/users/registration/confirm?token=" + token;
+        String message = "To continue registration use link below:";
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(addressToSend);
         email.setSubject(MAIL_SUBJECT);
