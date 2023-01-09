949035648cdcf7a9acab063d32a1c811ce563985
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-09 23:03:05 +0100

    interface was renamed

--- MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/repository/api/IProfileRepository.java
+++ MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/repository/api/IProfileRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+public interface IProfileRepository extends JpaRepository<Profile, UUID> {
 }
