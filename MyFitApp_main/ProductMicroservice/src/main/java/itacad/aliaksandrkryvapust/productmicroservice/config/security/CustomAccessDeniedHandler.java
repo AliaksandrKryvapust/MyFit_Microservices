13b3911c9fbc6989c8eff0ffaa7541ac0f8a76ea
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-09 18:16:11 +0100

    new exception handling

---
+++ MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/config/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,19 @@
+package itacad.aliaksandrkryvapust.productmicroservice.config.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setHeader("content-type", "application/json");
+        response.getWriter().write("This authorization token is prohibited from making requests to this address");
+    }
+}
---
+++ MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/config/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package itacad.aliaksandrkryvapust.productmicroservice.config.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("content-type", "application/json");
+        response.getWriter().write("To make a request to this address, you need to transfer an authorization token");
+    }
+}
