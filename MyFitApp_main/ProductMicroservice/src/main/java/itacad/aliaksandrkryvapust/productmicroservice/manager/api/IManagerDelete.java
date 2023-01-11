1c3c9754397cf9bff07ea73921b9257833ed7633
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-11 11:00:57 +0100

    method contract were changed

--- MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/manager/api/IManagerDelete.java
+++ MyFitApp_main/ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/manager/api/IManagerDelete.java
@@ -1,8 +1,7 @@
 package itacad.aliaksandrkryvapust.productmicroservice.manager.api;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 public interface IManagerDelete {
-    void delete(UUID id, HttpServletRequest request);
+    void delete(UUID id);
 }
