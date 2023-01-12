2e595842bd07228dc7d5d82bcf0c338b8fc9cefb
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-12 11:09:48 +0100

    new method was added to repository

--- MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/repository/api/IAuditRepository.java
+++ MyFitApp_main/AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/repository/api/IAuditRepository.java
@@ -3,9 +3,9 @@ package itacad.aliaksandrkryvapust.auditmicroservice.repository.api;
 import itacad.aliaksandrkryvapust.auditmicroservice.repository.entity.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface IAuditRepository extends JpaRepository<Audit, UUID> {
-    Optional<Audit> findByUuid(UUID id);
+    List<Audit> findByUuid(UUID id);
 }
