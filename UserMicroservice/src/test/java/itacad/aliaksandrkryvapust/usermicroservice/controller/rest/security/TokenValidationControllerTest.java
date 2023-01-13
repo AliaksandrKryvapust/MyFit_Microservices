e971d373ba5da779f5b0ec4f1858fbd0ed5b1502
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-13 22:28:53 +0100

    init

---
+++ UserMicroservice/src/test/java/itacad/aliaksandrkryvapust/usermicroservice/controller/rest/security/TokenValidationControllerTest.java
@@ -0,0 +1,12 @@
+package itacad.aliaksandrkryvapust.usermicroservice.controller.rest.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenValidationControllerTest {
+
+    @Test
+    void validateToken() {
+    }
+}
