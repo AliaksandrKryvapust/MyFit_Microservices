e84b1596a0277c952701af17b67e9b2b91b5abf0
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-01-15 19:02:44 +0100

    constants were added

--- UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/Constants.java
+++ UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/Constants.java
@@ -3,6 +3,9 @@ package itacad.aliaksandrkryvapust.usermicroservice.core;
 public class Constants {
     public static final String ENCODING = "UTF-8";
     public static final String AUDIT_URI = "http://localhost:8081/api/v1/audit";
+    public static final String IP_URI = "http://165.22.64.67";
+    public static final String PORT_URI = ":8090";
+    public static final String TOKEN_URI = "/api/v1/users/registration/confirm?token=";
     public static final String TOKEN_HEADER = "audit_access_token";
     public static final String MAIL_SUBJECT = "Registration Confirmation";
 }
