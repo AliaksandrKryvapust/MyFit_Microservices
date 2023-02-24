9c23586463c07dee90ef730cfb8e91476cfed4cd
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-24 14:53:48 +0100

    fields were renamed

--- UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/dto/input/UserDtoInput.java
+++ UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/core/dto/input/UserDtoInput.java
@@ -18,14 +18,14 @@ import javax.validation.constraints.Size;
 public class UserDtoInput {
     @NotNull(message = "username cannot be null")
     @Size(min = 2, max = 50, message = "username should contain from 2 to 50 letters")
-    private final String nick;
+    private final String username;
     @NotNull(message = "password cannot be null")
     @Size(min = 2, max = 200, message = "password should contain from 2 to 200 letters")
     private final String password;
     @NotNull(message = "email cannot be null")
     @NotEmpty
     @IValidEmail(message = "email is not valid")
-    private final String mail;
+    private final String email;
     @NotNull(message = "user role cannot be null")
     @IValidEnum(enumClass = EUserRole.class, message = "user role does not match")
     private final String role;
