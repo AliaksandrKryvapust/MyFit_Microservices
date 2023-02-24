0ae0cbdd520045696d09ce687cbf110a907cafae
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-24 14:57:54 +0100

    email verification event was updated

--- UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/event/EmailVerificationEvent.java
+++ UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/event/EmailVerificationEvent.java
@@ -6,19 +6,13 @@ import lombok.Getter;
 import lombok.NonNull;
 import org.springframework.context.ApplicationEvent;
 
-import java.util.Locale;
-
 @Builder
 @Getter
 public class EmailVerificationEvent extends ApplicationEvent {
-    private final @NonNull String appUrl;
-    private final @NonNull Locale locale;
     private final @NonNull User user;
 
-    public EmailVerificationEvent(@NonNull String appUrl, @NonNull Locale locale, @NonNull User user) {
+    public EmailVerificationEvent(@NonNull User user) {
         super(user);
-        this.appUrl = appUrl;
-        this.locale = locale;
         this.user = user;
     }
 }
