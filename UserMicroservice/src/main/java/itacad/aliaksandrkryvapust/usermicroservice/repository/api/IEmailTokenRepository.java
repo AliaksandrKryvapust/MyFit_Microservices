091634ca6c263ca18aba36802f864a952437cdb0
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-24 22:18:14 +0100

    repositories were updated

--- UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/repository/api/IEmailTokenRepository.java
+++ UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/repository/api/IEmailTokenRepository.java
@@ -2,9 +2,12 @@ package itacad.aliaksandrkryvapust.usermicroservice.repository.api;
 
 import itacad.aliaksandrkryvapust.usermicroservice.repository.entity.EmailToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IEmailTokenRepository extends JpaRepository<EmailToken, UUID> {
-    EmailToken getEmailTokenByToken(String token);
+    @Transactional(readOnly = true)
+    Optional<EmailToken> getEmailTokenByToken(String token);
 }
--- UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/repository/api/IUserRepository.java
+++ UserMicroservice/src/main/java/itacad/aliaksandrkryvapust/usermicroservice/repository/api/IUserRepository.java
@@ -2,14 +2,13 @@ package itacad.aliaksandrkryvapust.usermicroservice.repository.api;
 
 import itacad.aliaksandrkryvapust.usermicroservice.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
-
-@Repository
 public interface IUserRepository extends JpaRepository<User, UUID> {
+    @Transactional(readOnly = true)
     Optional<User> findByEmail(String email);
 }
 
