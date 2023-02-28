97f886bf9a1ed978eb31778675685c591d49bc42
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-28 19:28:11 +0100

    manager interface was updated

--- ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/manager/api/IManagerUpdate.java
+++
@@ -1,7 +0,0 @@
-package itacad.aliaksandrkryvapust.productmicroservice.manager.api;
-
-import java.util.UUID;
-
-public interface IManagerUpdate<TYPE, TYPE2> {
-    TYPE update(TYPE2 type, UUID id, Long version);
-}
