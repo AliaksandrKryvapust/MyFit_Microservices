c54b1600791f6f0e714834041585f19cc7be5935
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-02-28 15:06:55 +0100

    new mapper was added

---
+++ ProductMicroservice/src/main/java/itacad/aliaksandrkryvapust/productmicroservice/core/mapper/ParamsMapper.java
@@ -0,0 +1,28 @@
+package itacad.aliaksandrkryvapust.productmicroservice.core.mapper;
+
+import itacad.aliaksandrkryvapust.productmicroservice.core.dto.export.ParamsDto;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ParamsMapper {
+
+    public ParamsDto getParamsDto(HttpServletRequest request) {
+        final LocalDate dateFrom = LocalDate.parse(request.getHeader("from"));
+        final LocalDate dateTo = LocalDate.parse(request.getHeader("to"));
+        final UUID userId = UUID.fromString(request.getHeader("id"));
+        return ParamsDto.builder()
+                .from(dateFrom.atStartOfDay().toInstant(ZoneOffset.UTC))
+                .to(dateTo.atTime(LocalTime.MAX).toInstant(ZoneOffset.UTC))
+                .userId(userId)
+                .build();
+    }
+}
