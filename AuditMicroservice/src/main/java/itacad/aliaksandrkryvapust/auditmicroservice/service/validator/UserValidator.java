45c5ae1fa1dccbea405e1a68795b611ad36a4b00
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-06 16:33:15 +0100

    validation fix

--- AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/service/validator/UserValidator.java
+++ AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/service/validator/UserValidator.java
@@ -9,14 +9,13 @@ public class UserValidator implements IUserValidator {
     @Override
     public void validateEntity(User entityToSave) {
         checkId(entityToSave);
-        checkUsername(entityToSave);
         checkEmail(entityToSave);
         checkUserRole(entityToSave);
     }
 
 
     private void checkId(User user) {
-        if (user.getId() != null) {
+        if (user.getId() == null) {
             throw new IllegalArgumentException("User id is empty for user: " + user);
         }
     }
@@ -34,16 +33,6 @@ public class UserValidator implements IUserValidator {
         char[] chars = user.getEmail().toCharArray();
         if (chars.length < 2 || chars.length > 50) {
             throw new IllegalArgumentException("email should contain from 2 to 50 letters for user:" + user);
-        }
-    }
-
-    private void checkUsername(User user) {
-        if (user.getUsername() == null || user.getUsername().isBlank()) {
-            throw new IllegalArgumentException("username is not valid for user:" + user);
-        }
-        char[] chars = user.getUsername().toCharArray();
-        if (chars.length < 2 || chars.length > 50) {
-            throw new IllegalArgumentException("username should contain from 2 to 50 letters for user:" + user);
         }
     }
 }
