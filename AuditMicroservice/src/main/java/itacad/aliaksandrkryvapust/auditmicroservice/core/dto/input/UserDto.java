2462a4a4cdc118d69623a461dfe28b5e435f663e
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-07 18:10:36 +0100

    dto`s were changed

--- AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/core/dto/input/UserDto.java
+++ AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/core/dto/input/UserDto.java
@@ -5,12 +5,10 @@ import itacad.aliaksandrkryvapust.auditmicroservice.repository.entity.EUserRole;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
-import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.Instant;
 
 @Builder
 @Data
@@ -18,18 +16,10 @@ import java.time.Instant;
 public class UserDto {
     @NotBlank(message = "id cannot be blank")
     private final String id;
-    @Nullable
-    private final String username;
     @NotNull(message = "email cannot be null")
     @Size(min = 2, max = 100, message = "email should contain from 2 to 100 letters")
     private final String email;
     @NotNull(message = "user role cannot be null")
     @IValidEnum(enumClass = EUserRole.class, message = "user role does not match")
     private final String role;
-    @Nullable
-    private final String status;
-    @Nullable
-    private final Instant dtCreate;
-    @Nullable
-    private final Instant dtUpdate;
 }
--- AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/core/dto/output/AuditDtoOutput.java
+++ AuditMicroservice/src/main/java/itacad/aliaksandrkryvapust/auditmicroservice/core/dto/output/AuditDtoOutput.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import java.time.Instant;
+
 @Data
 @Builder
 public class AuditDtoOutput {
@@ -12,4 +14,5 @@ public class AuditDtoOutput {
     private final @NonNull UserDtoOutput user;
     private final @NonNull String text;
     private final @NonNull String type;
+    private final @NonNull Instant dtCreate;
 }
