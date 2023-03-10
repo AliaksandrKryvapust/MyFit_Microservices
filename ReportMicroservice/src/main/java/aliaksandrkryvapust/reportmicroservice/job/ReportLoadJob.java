00b10652171fdf9c612b3f830f727ce963784d89
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-10 12:53:16 +0100

    report load job was refactored

--- ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/job/ReportLoadJob.java
+++ ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/job/ReportLoadJob.java
@@ -2,11 +2,13 @@ package aliaksandrkryvapust.reportmicroservice.job;
 
 import aliaksandrkryvapust.reportmicroservice.core.dto.job.RecordDto;
 import aliaksandrkryvapust.reportmicroservice.core.dto.output.microservices.EType;
+import aliaksandrkryvapust.reportmicroservice.core.dto.poi.XlsxRecord;
 import aliaksandrkryvapust.reportmicroservice.core.mapper.poi.XlsxRecordMapper;
-import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.EStatus;
+import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
 import aliaksandrkryvapust.reportmicroservice.service.api.IReportService;
 import aliaksandrkryvapust.reportmicroservice.service.api.IXlsxRecordService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -26,6 +28,7 @@ import static aliaksandrkryvapust.reportmicroservice.core.Constants.JOB_IMPORT_U
 import static aliaksandrkryvapust.reportmicroservice.core.Constants.TOKEN_HEADER;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class ReportLoadJob implements Job {
     private static final String jwtSecret = "NDQ1ZjAzNjQtMzViZi00MDRjLTljZjQtNjNjYWIyZTU5ZDYw";
@@ -33,43 +36,38 @@ public class ReportLoadJob implements Job {
     private final IXlsxRecordService xlsxRecordService;
     private final XlsxRecordMapper recordMapper;
 
-    public ReportLoadJob(IReportService reportService, IXlsxRecordService xlsxRecordService, XlsxRecordMapper recordMapper) {
-        this.reportService = reportService;
-        this.xlsxRecordService = xlsxRecordService;
-        this.recordMapper = recordMapper;
-    }
-
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         Report report = new Report();
         try {
             log.info("Report Job started");
             UUID id;
-            try {
-                report = this.reportService.getReport(EStatus.LOADED, EType.JOURNAL_FOOD).orElseThrow();
-                id = report.getId();
-                log.info("New task was added");
-            } catch (NoSuchElementException e) {
-                log.info("Report job, there is no data to work with");
-                return;
-            }
-            this.setProgressStatus(report, EStatus.PROGRESS, "Request was prepared");
-            Mono<List<RecordDto>> resp = this.prepareRequest(report);
-            report = this.reportService.getWithoutCredentialsCheck(id);
-            List<RecordDto> records = resp.blockOptional().orElseThrow();
+            report = reportService.getReport(EStatus.LOADED, EType.JOURNAL_FOOD)
+                    .orElseThrow(NoSuchElementException::new);
+            id = report.getId();
+            log.info("New task was added");
+            setProgressStatus(report, EStatus.PROGRESS, "Request was prepared");
+            Mono<List<RecordDto>> resp = prepareRequest(report);
+            report = reportService.getWithoutCredentialsCheck(id);
+            List<RecordDto> records = resp.blockOptional().orElseThrow(IllegalStateException::new);
             log.info("Data from response was extracted");
-            this.saveRecordAsFile(report, records);
+            saveRecordAsFile(report, records);
+        } catch (NoSuchElementException e) {
+            log.info("Report job, there is no data to work with");
+        } catch (IllegalStateException e) {
+            log.error("Can`t decode data from response");
         } catch (Exception e) {
             log.error(e.getMessage());
             report.setStatus(EStatus.ERROR);
-            this.reportService.save(report);
+            reportService.save(report);
         }
     }
 
     private void saveRecordAsFile(Report report, List<RecordDto> records) throws IOException {
-        byte[] convertedFile = xlsxRecordService.getRecordXlsData(recordMapper.listInputMapping(records));
+        List<XlsxRecord> recordList = recordMapper.listInputMapping(records);
+        byte[] convertedFile = xlsxRecordService.getRecordXlsData(recordList);
         report.setFileValue(convertedFile);
-        this.setProgressStatus(report, EStatus.DONE, "Report Job finished");
+        setProgressStatus(report, EStatus.DONE, "Report Job finished");
     }
 
     private Mono<List<RecordDto>> prepareRequest(Report report) {
@@ -80,7 +78,8 @@ public class ReportLoadJob implements Job {
                 .header("from", report.getParams().getStart().toString())
                 .header("to", report.getParams().getFinish().toString())
                 .header("id", report.getUser().getUserId().toString())
-                .retrieve().bodyToMono(new ParameterizedTypeReference<>() {
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<>() {
                 });
         log.info("Response was returned");
         return resp;
@@ -88,7 +87,7 @@ public class ReportLoadJob implements Job {
 
     private void setProgressStatus(Report report, EStatus progress, String logInfo) {
         report.setStatus(progress);
-        this.reportService.save(report);
+        reportService.save(report);
         log.info(logInfo);
     }
 }
