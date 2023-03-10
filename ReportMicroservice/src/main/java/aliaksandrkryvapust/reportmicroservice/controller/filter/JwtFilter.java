c1788fe01e241e38d4fc4188e5f41e63d19bdca7
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-10 11:49:07 +0100

    jwt filter was refactored

--- ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/controller/filter/JwtFilter.java
+++ ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/controller/filter/JwtFilter.java
@@ -5,6 +5,7 @@ import aliaksandrkryvapust.reportmicroservice.core.mapper.microservices.UserMapp
 import aliaksandrkryvapust.reportmicroservice.core.security.MyUserDetails;
 import aliaksandrkryvapust.reportmicroservice.core.security.UserPrincipal;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -25,38 +26,34 @@ import static aliaksandrkryvapust.reportmicroservice.core.Constants.TOKEN_VERIFI
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final UserMapper userMapper;
-
-    public JwtFilter(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String requestTokenHeader = request.getHeader(AUTHORIZATION);
-        TokenValidationDto validationDto = validateTokenThroughtRequest(requestTokenHeader);
+        TokenValidationDto validationDto = validateTokenThroughRequest(requestTokenHeader);
         if (validationDto.getAuthenticated()) {
-            this.prepareAuthenticationToken(request, validationDto);
+            prepareAuthenticationToken(request, validationDto);
             filterChain.doFilter(request, response);
-        } else {
-            logger.warn("Access denied");
         }
     }
 
     @NonNull
-    private TokenValidationDto validateTokenThroughtRequest(String requestTokenHeader) {
+    private TokenValidationDto validateTokenThroughRequest(String requestTokenHeader) {
         WebClient client = WebClient.create(TOKEN_VERIFICATION_URI);
         Mono<TokenValidationDto> resp = client.get()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .header(AUTHORIZATION, requestTokenHeader)
-                .retrieve().bodyToMono(TokenValidationDto.class);
+                .retrieve()
+                .bodyToMono(TokenValidationDto.class);
         return resp.blockOptional().orElseThrow();
     }
 
     private void prepareAuthenticationToken(HttpServletRequest request, TokenValidationDto validationDto) {
-        UserPrincipal userPrincipal = this.userMapper.inputValidationMapping(validationDto);
+        UserPrincipal userPrincipal = userMapper.inputValidationMapping(validationDto);
         MyUserDetails userDetails = new MyUserDetails(userPrincipal);
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
