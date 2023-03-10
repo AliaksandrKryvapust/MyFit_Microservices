42ee44216c5a63dfb84ab7e2b4444a732048a90f
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-10 15:34:07 +0100

    report repository was refactored

--- ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/repository/api/IReportRepository.java
+++ ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/repository/api/IReportRepository.java
@@ -1,23 +1,27 @@
 package aliaksandrkryvapust.reportmicroservice.repository.api;
 
 import aliaksandrkryvapust.reportmicroservice.core.dto.output.microservices.EType;
-import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.EStatus;
+import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface IReportRepository extends JpaRepository<Report, UUID> {
+    @Transactional(readOnly = true)
     @Query("SELECT new Report(rep.fileValue) FROM Report rep WHERE rep.id =?1")
     Report fileExport(UUID id);
 
+    @Transactional(readOnly = true)
     Page<Report> findAllByUser_Username(Pageable pageable, String username);
 
+    @Transactional(readOnly = true)
     Optional<Report> findByStatusAndType(EStatus status, EType type);
+    @Transactional(readOnly = true)
+    Optional<Report> findByIdAndUser_Username(UUID id, String username);
 }
