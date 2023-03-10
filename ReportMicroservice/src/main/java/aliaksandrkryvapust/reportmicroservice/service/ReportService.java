809dcce43537638b600be0589346ffd0ee74007f
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2023-03-10 16:14:24 +0100

    report manager was refactored

--- ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/service/ReportService.java
+++ ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/service/ReportService.java
@@ -1,13 +1,24 @@
 package aliaksandrkryvapust.reportmicroservice.service;
 
+import aliaksandrkryvapust.reportmicroservice.core.dto.input.ParamsDto;
+import aliaksandrkryvapust.reportmicroservice.core.dto.output.ReportDtoOutput;
+import aliaksandrkryvapust.reportmicroservice.core.dto.output.microservices.AuditDto;
 import aliaksandrkryvapust.reportmicroservice.core.dto.output.microservices.EType;
+import aliaksandrkryvapust.reportmicroservice.core.dto.output.pages.PageDtoOutput;
+import aliaksandrkryvapust.reportmicroservice.core.mapper.ReportMapper;
+import aliaksandrkryvapust.reportmicroservice.core.mapper.microservices.AuditMapper;
+import aliaksandrkryvapust.reportmicroservice.core.security.MyUserDetails;
 import aliaksandrkryvapust.reportmicroservice.repository.api.IReportRepository;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.EStatus;
 import aliaksandrkryvapust.reportmicroservice.repository.entity.Report;
+import aliaksandrkryvapust.reportmicroservice.service.api.IAuditManager;
+import aliaksandrkryvapust.reportmicroservice.service.api.IReportManager;
 import aliaksandrkryvapust.reportmicroservice.service.api.IReportService;
+import aliaksandrkryvapust.reportmicroservice.service.validator.api.IReportValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -16,8 +27,13 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class ReportService implements IReportService {
+public class ReportService implements IReportService, IReportManager {
+    private final static String REPORT_POST = "New request for report was created";
     private final IReportRepository reportRepository;
+    private final ReportMapper reportMapper;
+    private final IReportValidator reportValidator;
+    private final AuditMapper auditMapper;
+    private final IAuditManager auditManager;
 
     @Override
     public Report save(Report report) {
@@ -42,5 +58,31 @@ public class ReportService implements IReportService {
     @Override
     public Optional<Report> getReport(EStatus status, EType type) {
         return reportRepository.findByStatusAndType(status, type);
+    }
+
+    @Override
+    public void saveDto(ParamsDto paramsDto, EType type) {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Report entityToSave = reportMapper.inputMapping(paramsDto, type, userDetails);
+        reportValidator.validateEntity(entityToSave);
+        Report report = save(entityToSave);
+        prepareAudit(report, userDetails, REPORT_POST);
+    }
+
+    @Override
+    public PageDtoOutput<ReportDtoOutput> getDto(Pageable pageable) {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Page<Report> page = get(pageable, userDetails.getUsername());
+        return reportMapper.outputPageMapping(page);
+    }
+
+    @Override
+    public byte[] getReportFile(UUID id) {
+        return exportFile(id);
+    }
+
+    private void prepareAudit(Report report, MyUserDetails userDetails, String method) {
+        AuditDto auditDto = auditMapper.reportOutputMapping(report, userDetails, method);
+        auditManager.audit(auditDto);
     }
 }
--- ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/service/api/IReportManager.java
+++ ReportMicroservice/src/main/java/aliaksandrkryvapust/reportmicroservice/service/api/IReportManager.java
@@ -1,10 +1,11 @@
-package aliaksandrkryvapust.reportmicroservice.manager.api;
+package aliaksandrkryvapust.reportmicroservice.service.api;
 
 import aliaksandrkryvapust.reportmicroservice.core.dto.input.ParamsDto;
 import aliaksandrkryvapust.reportmicroservice.core.dto.output.ReportDtoOutput;
+import aliaksandrkryvapust.reportmicroservice.service.api.IManager;
 
 import java.util.UUID;
 
-public interface IReportManager extends IManager<ReportDtoOutput, ParamsDto>{
+public interface IReportManager extends IManager<ReportDtoOutput, ParamsDto> {
     byte[] getReportFile(UUID id);
 }
